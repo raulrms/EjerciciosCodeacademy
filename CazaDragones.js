2825fa59bc60e304b65220c0b6df9a17b6d6f481
Raúl Martínez <raulrms@yahoo.es>
2015-05-07 10:25:07 +0200

    Update CazaDragones.js

--- CazaDragones.js
+++ CazaDragones.js
@@ -5,5 +5,18 @@ var lastimaEsteRound = Math.floor(Math.random()*5 + 1);
 perdidaTotal=0;
 
 while(atacar) {
+    if (vosAcertas) {
+        console.log("¡Vos acertás!");
+        perdidaTotal += lastimaEsteRound;
+        
+        if (perdidaTotal >= 4) {
+            console.log("¡Ganaste!");
+            atacar = false;
+        } else {
+            vosAcertas = Math.floor(Math.random() * 2);
+        }       
+    } else {
+        console.log("¡Fallaste!");
+    }
     atacar = false;
 }
